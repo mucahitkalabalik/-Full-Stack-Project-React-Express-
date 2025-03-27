5299fa76f5c507df25a41d2d92c6c1233e16cc15
Mücahit <71331160+mucahitkalabalik@users.noreply.github.com>
2025-03-27 04:00:19 +0300

    Update axios.js

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // API URL'ni buraya ekle
-
+const API_BASE_URL = "http://localhost:5000/api";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
